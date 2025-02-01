53e975c6243c8adc1e36669ece891047fcc1fb76
Pierre Gallois <pierre.gallois3.14@gmail.com>
2025-02-01 17:55:40 +0100

    Début d'une barre de contrôles

--- proof-of-concept/app-test/gui/render.py
+++ proof-of-concept/app-test/gui/render.py
@@ -1,39 +1,24 @@
 import flet as ft
 from flet import canvas as cv
 
-from .spirograph import render_spirograph
+
+from .bar import settings_bar
 from .state_provider import centered_canvas
 
 def main(page: ft.Page):
     page.title = "Proof of Concept"
 
     cp = centered_canvas(page)
-
-    render_spirograph(
-        cp,
-        (0,0),
-        125,
-        200,
-        0.4,
-        0.25
-    )
-
-    render_spirograph(
-        cp,
-        (0,0),
-        50,
-        20,
-        0.1,
-        0.4
-    )
+    appbar = settings_bar(page, cp)
 
     page.add(
+        appbar,
         ft.Container(
             cp,
             border_radius=5,
             width=float("inf"),
             expand=True,
-        )
+        ),
     )
 
 def render():
