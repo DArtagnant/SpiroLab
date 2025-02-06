97b9e8da73af22bf91392a743c65e4c8bc068c1c
Pierre Gallois <pierre.gallois3.14@gmail.com>
2025-02-06 16:40:27 +0100

    Docstring

--- proof-of-concept/app-test/audio/getter.py
+++ proof-of-concept/app-test/audio/getter.py
@@ -5,4 +5,7 @@ def from_file(obj):
     return read(obj)
 
 def test_audio():
-    return *from_file(open_binary("app-test.static_data", "test_audio.wav")), 2
+    """
+    Renvoie (data, sample_rate, channel_number)
+    """
+    return *from_file(open_binary("static_data", "test_audio.wav")), 2
