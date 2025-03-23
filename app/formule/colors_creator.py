3303b436d0aefd0e4fcf64e48a756574809177c2
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2025-03-23 13:32:04 +0100

    spiros en home page

---
+++ app/formule/colors_creator.py
@@ -0,0 +1,56 @@
+from colorsys import hsv_to_rgb
+
+def hex_to_rgb(hex_color):
+    """Convert a hex color (e.g. "#ACDDDE") to an (R, G, B) tuple."""
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+def rgb_to_hex(rgb):
+    """Convert an (R, G, B) tuple to a hex color string."""
+    return "#{:02X}{:02X}{:02X}".format(*rgb)
+
+def interpolate_colors(color1, color2, t):
+    """
+    Interpolate between two RGB colors.
+    t should be between 0 (color1) and 1 (color2).
+    """
+    return (
+        int(color1[0] + (color2[0] - color1[0]) * t),
+        int(color1[1] + (color2[1] - color1[1]) * t),
+        int(color1[2] + (color2[2] - color1[2]) * t),
+    )
+
+def smooth_color_generator(colors, steps, easing=None):
+    """
+    Create a generator that yields colors as hex strings.
+    
+    Parameters:
+      colors: iterable of hex color strings.
+      steps: the number of intermediate colors between each pair.
+      
+    The generator smoothly interpolates between colors and finally connects 
+    the last color back to the first.
+    """
+
+    if easing is None:
+        easing = lambda t: t
+
+    rgb_colors = [hex_to_rgb(c) for c in colors]
+    n = len(rgb_colors)
+    
+    for i in range(n):
+        start = rgb_colors[i]
+        end = rgb_colors[(i+1) % n]
+        for step in range(steps):
+            t = step / steps
+            rgb_interp = interpolate_colors(start, end, easing(t))
+            yield rgb_to_hex(rgb_interp)
+    yield rgb_to_hex(rgb_colors[0])
+
+
+def progressive_color_arc_en_ciel(nb_points):
+    hue = 0.0
+    pas = 3 / nb_points # 2 = nombre de cycles
+    while True:
+        yield "#{}{}{}".format(*map(lambda n:hex(int(255*n))[2:].zfill(2), hsv_to_rgb(hue, 1, 1)))
+        hue = (hue + pas)%1.0
---
+++ app/formule/easing.py
@@ -0,0 +1,16 @@
+from math import sin, cos, pi
+
+def ease_in_out_cubic(t):
+    if t < 0.5:
+        return 4 * t**3
+    else:
+        return 1 - ((-2 * t + 2) ** 3) / 2
+
+def ease_in_cubic(t):
+    return t**3
+
+def ease_in_sine(t):
+    return 1 - cos((t * pi) / 2)
+
+def ease_out_sine(t):
+    return sin((t * pi) / 2)
