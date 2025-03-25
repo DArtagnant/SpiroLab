fe719f4be7ff640b01280cb29c5630c4fdcc7588
Pierre Gallois <pierre.gallois3.14@gmail.com>
2025-03-25 11:22:13 +0100

    On retire temporairement la home_page pour tester

--- app/gui/render.py
+++ app/gui/render.py
@@ -2,21 +2,25 @@ import flet as ft
 from time import sleep
 
 from .home_page import home_page
+from .bar import settings_bar
 from .centered_canvas import centered_canvas
 
 def main(page: ft.Page):
     page.title = "SpiroLab"
 
-    # cp = centered_canvas(page)
-    home_page(page)
-    # page.add(
-    #     # ft.Container(
-    #     #     cp,
-    #     #     border_radius=5,
-    #     #     width=float("inf"),
-    #     #     expand=True,
-    #     # ),
-    # )
+    cp = centered_canvas(page)
+    bar = settings_bar(page, cp)
+    # home_page(page) : TODO, fix le passage de la home_page au reste
+    page.add(
+        bar,
+        ft.Container(
+            cp,
+            border_radius=5,
+            width=float("inf"),
+            expand=True,
+        ),
+    )
+    
 
 
 
