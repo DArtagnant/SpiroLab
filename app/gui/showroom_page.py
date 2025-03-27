1eb32c5fe5c63fb450b80e076cb526b7cfc1a58e
Pierre Gallois <pierre.gallois3.14@gmail.com>
2025-03-27 15:18:16 +0100

    Animation fadein fadeout

--- app/gui/showroom_page.py
+++ app/gui/showroom_page.py
@@ -6,11 +6,12 @@ from random import randint
 from formule import colors_creator
 from .spirograph import render_spirograph
 from time import sleep
+from formule import easing
 
 MAX_SPIROS_ON_SCREEN = 4
+MARGE = 50
 
-
-def compute_spirographs_from_wav(page, canvas, path):
+def compute_spirographs_from_wav(page, canvases, containers, path):
     # TODO : empêcher cette fonction de continuer à s'éxécuter quand on commence à faire autre chose, type afficher un autre spiro
     arrays = read_wav(path)
 
@@ -18,12 +19,14 @@ def compute_spirographs_from_wav(page, canvas, path):
     small_radii = normalize_around(arrays[1], 60, 50)
     large_frequencies = normalize_around(arrays[2], 45, 40).astype(int) # On s'assure de n'avoir que des entiers dans la liste
     small_frequencies = normalize_around(arrays[3], 35, 30).astype(int)
-    resolution = normalize_around(arrays[4], 35, 15) # TODO : vérifier que ces valeurs ne sont pas débiles
+    resolution = normalize_around(arrays[4], 60, 15) # TODO : vérifier que ces valeurs ne sont pas débiles
 
     current_nb_spiros = 0
+    j = 0
     for i in range(len(large_radii)):
         if page.current_view_name != "showroom":
             return
+        
         # Si le spirographe affiché n'est pas suffisemment différent du dernier, on passe
         # Permet de ne jamais avoir deux spirographes presque identiques d'affilée
         if (i != 0 and
@@ -32,11 +35,18 @@ def compute_spirographs_from_wav(page, canvas, path):
         ):
             continue
 
-        if current_nb_spiros%MAX_SPIROS_ON_SCREEN == 0:
-            canvas.remove_all()
+        while page.width is None or page.height is None:
+            print("double skipped frame")
+            page.update()
+            sleep(0.01)
+
+        before = j%len(canvases)    
+        now = (j+1)%len(canvases)
+        print((j+1)%len(canvases))
+        
         render_spirograph(
-            canvas,
-            (randint(500, 1000), randint(-200, 200)), # Position aléatoire
+            canvases[before],
+            (randint(MARGE, int(page.width) - MARGE), randint(int(-page.height) + MARGE, -MARGE)), # Position aléatoire
 
             large_radii[i],
             small_radii[i],
@@ -47,28 +57,43 @@ def compute_spirographs_from_wav(page, canvas, path):
             iter_color = colors_creator.gen_random_color_scheme()
         )
 
-        sleep(0.5)
-        canvas.draw_once()
+        canvases[before].draw_once()
+        
+        n_steps = 500
+        for step in range(n_steps):
+            t = step / n_steps
+            containers[before].opacity = easing.ease_in_sine(t)
+            containers[now].opacity = 1 - easing.ease_out_sine(t)
+            page.update()
+            sleep(0.01)
+
+        containers[before].opacity = 1
+        canvases[now].remove_all()
         page.update()
         current_nb_spiros = (current_nb_spiros + 1)%MAX_SPIROS_ON_SCREEN
-       
+        j += 1
 
 def showroom_page(page: ft.Page, audio_path, custom_spiro) -> ft.View:
     page.current_view_name = "showroom"
-    cc = centered_canvas(page)
+    cc1 = centered_canvas(page)
+    cc2 = centered_canvas(page)
+    cc3 = centered_canvas(page)
+    cc4 = centered_canvas(page)
 
     showroom_page_view = ft.View(
         route= "/dessin",
         controls= [
-            ft.Container(
-                cc,
-                expand= True,
-            )
-        ]
+            ft.Stack([
+                ccc1 := ft.Container(cc1, expand=True),
+                ccc2 := ft.Container(cc2, expand=True),
+                ccc3 := ft.Container(cc3, expand=True),
+                ccc4 := ft.Container(cc4, expand=True),
+            ],
+            expand=True)
+        ],
     )
 
     #showroom_page_view.floating_action_button = ft.FloatingActionButton(text="Construire son propre spirograph", icon=ft.Icons.ARROW_RIGHT, on_click=custom_spiro)
-
-    page.run_thread(lambda: compute_spirographs_from_wav(page, cc, audio_path))
+    page.run_thread(lambda: compute_spirographs_from_wav(page, [cc1, cc2, cc3], [ccc1, ccc2, ccc3], audio_path))
     return showroom_page_view
 
